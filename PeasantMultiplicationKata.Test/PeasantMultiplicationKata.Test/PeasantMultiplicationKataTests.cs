89cca7addbc5470e2465f1d550e8802df6abfc1f
Adam Witko <esendexdev+adamwitko@gmail.com>
2013-06-06 17:33:17 +0100

    Initial commit

---
+++ PeasantMultiplicationKata.Test/PeasantMultiplicationKata.Test/PeasantMultiplicationKataTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+
+namespace PeasantMultiplicationKata.Test
+{
+    [TestFixture]
+    public class PeasantMultiplicationKataTests
+    {
+        private int _expected;
+        private int _x;
+        private int _y;
+        private int _result;
+
+        [SetUp]
+        public void GivenTenAndTwelveWhenMultiplying()
+        {
+            _x = 10;
+            _y = 12;
+
+            _expected = 120;
+
+            _result = PeasantMultiplication.Multiply(_x, _y);
+        }
+
+        [Test]
+        public void ThenTheResultIsOneHundredAndTwenty()
+        {
+
+            Assert.That(_result, Is.EqualTo(_expected));
+        }
+    }
+
+    public class PeasantMultiplication
+    {
+        public static int Multiply(int x, int y)
+        {
+            return 0;
+        }
+    }
+}
